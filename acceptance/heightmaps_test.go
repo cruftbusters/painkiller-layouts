c40be4ce78690ee1ade6b2eeea89d048c865116e
tyler <tyler@cruftbusters.com>
2021-10-30 12:53:50 -0500

    refactor - assert 404 not found for new endpoint

--- acceptance/heightmaps_test.go
+++ acceptance/heightmaps_test.go
@@ -10,10 +10,19 @@ func TestHeightmaps(t *testing.T) {
 		http.ListenAndServe(":8080", nil)
 	}()
 
-	t.Run("connect", func(t *testing.T) {
-		_, err := http.Get("http://localhost:8080/v1/heightmaps/deadbeef")
+	t.Run("get missing heightmap", func(t *testing.T) {
+		response, err := http.Get("http://localhost:8080/v1/heightmaps/deadbeef")
 		if err != nil {
 			t.Fatal("got error wanted no error", err)
 		}
+
+		assertStatusCode(t, response, 404)
 	})
+}
+
+func assertStatusCode(t testing.TB, response *http.Response, statusCode int) {
+	t.Helper()
+	if response.StatusCode != statusCode {
+		t.Fatalf("got status code %d want %d", response.StatusCode, statusCode)
+	}
 }
