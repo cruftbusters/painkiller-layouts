6ab16611623b0d215a4b677d6414a80f6372931a
tyler <tyler@cruftbusters.com>
2021-10-30 12:58:21 -0500

    refactor - extract assertNoError

--- acceptance/heightmaps_test.go
+++ acceptance/heightmaps_test.go
@@ -12,12 +12,15 @@ func TestHeightmaps(t *testing.T) {
 
 	t.Run("get missing heightmap", func(t *testing.T) {
 		response, err := http.Get("http://localhost:8080/v1/heightmaps/deadbeef")
-		if err != nil {
-			t.Fatal("got error wanted no error", err)
-		}
+		assertNoError(t, err)
 
 		assertStatusCode(t, response, 404)
 	})
+
+func assertNoError(t testing.TB, err error) {
+	if err != nil {
+		t.Fatal("got error wanted no error", err)
+	}
 }
 
 func assertStatusCode(t testing.TB, response *http.Response, statusCode int) {
