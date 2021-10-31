081b7228bfd40ffa9f6d74d2b2ce2f2d8347179b
tyler <tyler@cruftbusters.com>
2021-10-30 22:36:38 -0500

    refactor - move some common assertions, encoding + decoding into acceptance client

--- acceptance/client.go
+++ acceptance/client.go
@@ -4,24 +4,66 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+	"testing"
 
 	. "github.com/cruftbusters/painkiller-gallery/types"
 )
 
 type Client struct {
+	t       testing.TB
 	BaseUrl string
 }
 
-func (client Client) getMetadata() (*http.Response, error) {
-	return http.Get(client.baseUrl("/v1/heightmaps/deadbeef"))
+func (client Client) GetMetadata() Metadata {
+	response, err := http.Get(client.baseUrl("/v1/heightmaps/deadbeef"))
+	assertNoError(client.t, err)
+	assertStatusCode(client.t, response, 200)
+
+	return decodeMetadata(client.t, response)
 }
 
-func (client Client) post(metadata Metadata) (*http.Response, error) {
-	body := &bytes.Buffer{}
-	json.NewEncoder(body).Encode(metadata)
-	return http.Post(client.baseUrl("/v1/heightmaps"), "", body)
+func (client Client) GetMetadataExpectNotFound() {
+	response, err := http.Get(client.baseUrl("/v1/heightmaps/deadbeef"))
+	assertNoError(client.t, err)
+	assertStatusCode(client.t, response, 404)
+}
+
+func (client Client) Create(metadata Metadata) Metadata {
+	up := encodeMetadata(client.t, metadata)
+	response, err := http.Post(client.baseUrl("/v1/heightmaps"), "", up)
+	assertNoError(client.t, err)
+	assertStatusCode(client.t, response, 201)
+
+	return decodeMetadata(client.t, response)
 }
 
 func (client Client) baseUrl(path string) string {
 	return client.BaseUrl + path
+}
+
+func encodeMetadata(t testing.TB, metadata Metadata) *bytes.Buffer {
+	up := &bytes.Buffer{}
+	err := json.NewEncoder(up).Encode(metadata)
+	assertNoError(t, err)
+	return up
+}
+
+func assertNoError(t testing.TB, err error) {
+	if err != nil {
+		t.Fatal("got error wanted no error", err)
+	}
+}
+
+func assertStatusCode(t testing.TB, response *http.Response, statusCode int) {
+	t.Helper()
+	if response.StatusCode != statusCode {
+		t.Fatalf("got status code %d want %d", response.StatusCode, statusCode)
+	}
+}
+
+func decodeMetadata(t testing.TB, response *http.Response) Metadata {
+	down := &Metadata{}
+	err := json.NewDecoder(response.Body).Decode(down)
+	assertNoError(t, err)
+	return *down
 }
--- acceptance/heightmaps_test.go
+++ acceptance/heightmaps_test.go
@@ -1,7 +1,6 @@
 package acceptance
 
 import (
-	"encoding/json"
 	"net/http"
 	"testing"
 
@@ -14,61 +13,34 @@ func TestHeightmaps(t *testing.T) {
 		http.ListenAndServe(":8080", heightmap.Handler())
 	}()
 
-	client := Client{BaseUrl: "http://localhost:8080"}
+	client := Client{t: t, BaseUrl: "http://localhost:8080"}
 
 	t.Run("get missing heightmap", func(t *testing.T) {
-		response, err := client.getMetadata()
-		assertNoError(t, err)
-
-		assertStatusCode(t, response, 404)
+		client.GetMetadataExpectNotFound()
 	})
 
 	t.Run("create new heightmap", func(t *testing.T) {
-		postResponse, err := client.post(Metadata{
-			Size: "large",
-		})
-		assertNoError(t, err)
+		assertMetadata(t,
+			client.Create(Metadata{
+				Size: "large",
+			}),
+			Metadata{
+				Id:   "deadbeef",
+				Size: "large",
+			})
 
-		assertStatusCode(t, postResponse, 201)
-
-		assertBody(t, postResponse, Metadata{
-			Id:   "deadbeef",
-			Size: "large",
-		})
-
-		response, err := http.Get("http://localhost:8080/v1/heightmaps/deadbeef")
-		assertNoError(t, err)
-
-		assertStatusCode(t, response, 200)
-
-		assertBody(t, response, Metadata{
-			Id:   "deadbeef",
-			Size: "large",
-		})
+		assertMetadata(t,
+			client.GetMetadata(),
+			Metadata{
+				Id:   "deadbeef",
+				Size: "large",
+			})
 	})
 }
 
-func assertNoError(t testing.TB, err error) {
-	if err != nil {
-		t.Fatal("got error wanted no error", err)
-	}
-}
-
-func assertStatusCode(t testing.TB, response *http.Response, statusCode int) {
+func assertMetadata(t testing.TB, got Metadata, want Metadata) {
 	t.Helper()
-	if response.StatusCode != statusCode {
-		t.Fatalf("got status code %d want %d", response.StatusCode, statusCode)
-	}
-}
-
-func assertBody(t testing.TB, response *http.Response, want Metadata) {
-	t.Helper()
-	defer response.Body.Close()
-	got := &Metadata{}
-	if err := json.NewDecoder(response.Body).Decode(got); err != nil {
-		t.Fatal("got json error decoding body", err)
-	}
-	if *got != want {
+	if got != want {
 		t.Fatalf("got %#v want %#v", got, want)
 	}
 }
