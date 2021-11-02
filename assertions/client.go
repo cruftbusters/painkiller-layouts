ee4f10febdefaf8b31cd443ea88d817163cd88fc
tyler <tyler@cruftbusters.com>
2021-11-02 11:37:02 -0500

    refactor - add some consistency to client base url

--- assertions/client.go
+++ assertions/client.go
@@ -10,15 +10,15 @@ import (
 
 type Client struct {
 	t       testing.TB
-	BaseUrl string
+	baseURL string
 }
 
 func NewClient(t testing.TB, baseURL string) Client {
-	return Client{t: t, BaseUrl: baseURL}
+	return Client{t: t, baseURL: baseURL}
 }
 
 func (client Client) GetMetadata(id string) Metadata {
-	response, err := http.Get(client.baseUrl("/v1/heightmaps/%s", id))
+	response, err := http.Get(client.baseURLF("/v1/heightmaps/%s", id))
 	AssertNoError(client.t, err)
 	AssertStatusCode(client.t, response, 200)
 
@@ -26,20 +26,20 @@ func (client Client) GetMetadata(id string) Metadata {
 }
 
 func (client Client) GetMetadataExpectNotFound() {
-	response, err := http.Get(client.baseUrl("/v1/heightmaps/deadbeef"))
+	response, err := http.Get(client.baseURLF("/v1/heightmaps/deadbeef"))
 	AssertNoError(client.t, err)
 	AssertStatusCode(client.t, response, 404)
 }
 
 func (client Client) Create(metadata Metadata) Metadata {
 	up := encode(client.t, metadata)
-	response, err := http.Post(client.baseUrl("/v1/heightmaps"), "", up)
+	response, err := http.Post(client.baseURLF("/v1/heightmaps"), "", up)
 	AssertNoError(client.t, err)
 	AssertStatusCode(client.t, response, 201)
 
 	return decode(client.t, response)
 }
 
-func (client Client) baseUrl(path string, a ...interface{}) string {
-	return client.BaseUrl + fmt.Sprintf(path, a...)
+func (client Client) baseURLF(path string, a ...interface{}) string {
+	return client.baseURL + fmt.Sprintf(path, a...)
 }
