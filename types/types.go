9766e07f7dd32a3af0e60d513981c449f6cabdb3
tyler <tyler@cruftbusters.com>
2021-11-09 11:17:15 -0600

    add position{top,left} to metadata

--- types/types.go
+++ types/types.go
@@ -1,12 +1,18 @@
 package types
 
 type Metadata struct {
-	Id       string `json:"id"`
-	Size     Size   `json:"size"`
-	ImageURL string `json:"imageURL"`
+	Id       string   `json:"id"`
+	Size     Size     `json:"size"`
+	Position Position `json:"position"`
+	ImageURL string   `json:"imageURL"`
 }
 
 type Size struct {
 	Width  int `json:"width"`
 	Height int `json:"height"`
+}
+
+type Position struct {
+	Top  float64 `json:"top"`
+	Left float64 `json:"left"`
 }
