796405063b18395cfd83f815ad885fb3fd986aea
tyler <tyler@cruftbusters.com>
2021-11-12 17:57:22 -0600

    remove metadata.position

--- types/types.go
+++ types/types.go
@@ -1,21 +1,15 @@
 package types
 
 type Metadata struct {
-	Id       string   `json:"id"`
-	Size     Size     `json:"size"`
-	Position Position `json:"position"`
-	Bounds   Bounds   `json:"bounds"`
-	ImageURL string   `json:"imageURL"`
+	Id       string `json:"id"`
+	Size     Size   `json:"size"`
+	Bounds   Bounds `json:"bounds"`
+	ImageURL string `json:"imageURL"`
 }
 
 type Size struct {
 	Width  int `json:"width"`
 	Height int `json:"height"`
-}
-
-type Position struct {
-	Left float64 `json:"left"`
-	Top  float64 `json:"top"`
 }
 
 type Bounds struct {
