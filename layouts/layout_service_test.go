93f5694344bb2200fdd17f17366f355eb61c5258
tyler <tyler@cruftbusters.com>
2021-11-14 21:17:01 -0600

    resilient cleanup in layouts service test

--- layouts/layout_service_test.go
+++ layouts/layout_service_test.go
@@ -29,14 +29,13 @@ func TestLayoutService(t *testing.T) {
 		stubUuidService.idQueue = []string{"the id"}
 
 		got := service.Create(Layout{})
+		defer func() { service.Delete("the id") }()
 		want := Layout{Id: "the id"}
 		AssertLayout(t, got, want)
 
 		layout, err := service.Get(got.Id)
 		AssertNoError(t, err)
 		AssertLayout(t, layout, got)
-
-		service.Delete("the id")
 	})
 
 	t.Run("create and get all heightmaps", func(t *testing.T) {
@@ -44,14 +43,13 @@ func TestLayoutService(t *testing.T) {
 		heightmapURL := "better not drop me"
 
 		service.Create(Layout{})
+		defer func() { service.Delete("first") }()
 		service.Create(Layout{HeightmapURL: heightmapURL})
+		defer func() { service.Delete("second") }()
 
 		got := service.GetAll(false)
 		want := []Layout{{Id: "first"}, {Id: "second", HeightmapURL: "better not drop me"}}
 		AssertLayoutsUnordered(t, got, want)
-
-		service.Delete("first")
-		service.Delete("second")
 	})
 
 	t.Run("patch missing map", func(t *testing.T) {
@@ -63,6 +61,7 @@ func TestLayoutService(t *testing.T) {
 		id, size, heightmapURL := "the id", Size{Width: 1, Height: 2}, "new heightmap url"
 		stubUuidService.idQueue = []string{id}
 		service.Create(Layout{Size: size})
+		defer func() { service.Delete(id) }()
 
 		got, err := service.Patch(id, Layout{HeightmapURL: heightmapURL})
 		AssertNoError(t, err)
@@ -72,21 +71,18 @@ func TestLayoutService(t *testing.T) {
 		got, err = service.Get(id)
 		AssertNoError(t, err)
 		AssertLayout(t, got, want)
-
-		service.Delete(id)
 	})
 
 	t.Run("filter for maps with no heightmap", func(t *testing.T) {
 		stubUuidService.idQueue = []string{"first", "second"}
 		withoutHeightmap := service.Create(Layout{})
+		defer func() { service.Delete(withoutHeightmap.Id) }()
 		withHeightmap := service.Create(Layout{HeightmapURL: "heightmap url"})
+		defer func() { service.Delete(withHeightmap.Id) }()
 		AssertLayouts(t,
 			service.GetAll(true),
 			[]Layout{withoutHeightmap},
 		)
-
-		service.Delete(withoutHeightmap.Id)
-		service.Delete(withHeightmap.Id)
 	})
 
 	t.Run("delete heightmap", func(t *testing.T) {
