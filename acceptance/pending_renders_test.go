5a917a854bb32b6677d3a07c01499494bb66c0e2
tyler <tyler@cruftbusters.com>
2021-11-20 23:37:19 -0600

    wip

--- acceptance/pending_renders_test.go
+++ acceptance/pending_renders_test.go
@@ -10,31 +10,34 @@ import (
 )
 
 func TestPendingRenders(t *testing.T) {
-	_, wsBaseURL := t2.TestServer(v1.Handler)
-	conn, _, err := websocket.DefaultDialer.Dial(wsBaseURL, nil)
-	t2.AssertNoError(t, err)
+	t.Run("ping every interval", func(t *testing.T) {
+		_, wsBaseURL := t2.TestServer(v1.Handler)
+		conn, _, err := websocket.DefaultDialer.Dial(wsBaseURL, nil)
+		t2.AssertNoError(t, err)
+		defer conn.Close()
 
-	go conn.ReadMessage()
+		go conn.ReadMessage()
 
-	signal := make(chan *struct{})
-	conn.SetPingHandler(func(string) error { signal <- nil; return nil })
+		signal := make(chan *struct{})
+		conn.SetPingHandler(func(string) error { signal <- nil; return nil })
 
-	one, five, six := time.After(time.Second), time.After(5*time.Second), time.After(6*time.Second)
-	select {
-	case <-signal:
-	case <-one:
-		t.Fatal("expected ping in less than one second")
-	}
+		one, five, six := time.After(time.Second), time.After(5*time.Second), time.After(6*time.Second)
+		select {
+		case <-signal:
+		case <-one:
+			t.Fatal("expected ping in less than one second")
+		}
 
-	select {
-	case <-signal:
-		t.Fatal("expected no ping in less than five seconds")
-	case <-five:
-	}
+		select {
+		case <-signal:
+			t.Fatal("expected no ping in less than five seconds")
+		case <-five:
+		}
 
-	select {
-	case <-signal:
-	case <-six:
-		t.Fatal("expected ping in less than six seconds")
-	}
+		select {
+		case <-signal:
+		case <-six:
+			t.Fatal("expected ping in less than six seconds")
+		}
+	})
 }
--- v1/pending_renders_controller_test.go
+++ v1/pending_renders_controller_test.go
@@ -9,33 +9,35 @@ import (
 )
 
 func TestPendingRendersController(t *testing.T) {
-	interval := time.Second
-	controller := &PendingRendersController{interval}
-	_, wsBaseURL := t2.TestController(controller)
-	conn, _, err := websocket.DefaultDialer.Dial(wsBaseURL, nil)
-	t2.AssertNoError(t, err)
+	t.Run("ping every interval", func(t *testing.T) {
+		interval := time.Second
+		controller := &PendingRendersController{interval}
+		_, wsBaseURL := t2.TestController(controller)
+		conn, _, err := websocket.DefaultDialer.Dial(wsBaseURL, nil)
+		t2.AssertNoError(t, err)
 
-	go conn.ReadMessage()
+		go conn.ReadMessage()
 
-	signal := make(chan *struct{})
-	conn.SetPingHandler(func(string) error { signal <- nil; return nil })
+		signal := make(chan *struct{})
+		conn.SetPingHandler(func(string) error { signal <- nil; return nil })
 
-	one, five, six := time.After(time.Second), time.After(interval), time.After(interval+time.Second)
-	select {
-	case <-signal:
-	case <-one:
-		t.Fatal("expected ping in less than one second")
-	}
+		one, five, six := time.After(time.Second), time.After(interval), time.After(interval+time.Second)
+		select {
+		case <-signal:
+		case <-one:
+			t.Fatal("expected ping in less than one second")
+		}
 
-	select {
-	case <-signal:
-		t.Fatalf("expected no ping in less than %s", interval)
-	case <-five:
-	}
+		select {
+		case <-signal:
+			t.Fatalf("expected no ping in less than %s", interval)
+		case <-five:
+		}
 
-	select {
-	case <-signal:
-	case <-six:
-		t.Fatalf("expected ping in less than %s", interval+time.Second)
-	}
+		select {
+		case <-signal:
+		case <-six:
+			t.Fatalf("expected ping in less than %s", interval+time.Second)
+		}
+	})
 }
